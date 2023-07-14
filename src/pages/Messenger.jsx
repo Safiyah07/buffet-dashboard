be552ae487379c69b7639e4cb56a5de0266ed6de
Safiyah <safiyahmasud@gmail.com>
2023-07-14 12:42:27 +0100

    message page contd

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { IoIosArrowDown } from 'react-icons/io'
+import { TbBookUpload, TbSettings } from 'react-icons/tb'
+import { LuVideo, LuPhoneCall, LuImage } from 'react-icons/lu'
 import Avatar1 from '../assets/Avatar1.png'
 import Avatar2 from '../assets/Avatar2.png'
 import Avatar3 from '../assets/Avatar3.png'
@@ -88,6 +90,60 @@ export const Messenger = () => {
 		},
 	])
 
+	// eslint-disable-next-line no-unused-vars
+	const [chatBox, setChatBox] = useState([
+		{
+			name: 'Henry Phillip',
+			image: (
+				<img
+					src={Avatar5}
+					className='w-10 h-10'
+				/>
+			),
+			time: '11:34 AM',
+			message: 'Hey, guys! How are sales going?',
+			status: 'Delivered',
+		},
+		{
+			name: 'Seline Kim',
+			image: (
+				<img
+					src={Avatar2}
+					className='w-10 h-10'
+				/>
+			),
+			time: '10:30 AM',
+			message:
+				'Not bad, but i think we could do better. We need to step up our game.',
+			status: 'Delivered',
+		},
+		{
+			name: 'Kevin Brown',
+			image: (
+				<img
+					src={Avatar1}
+					className='w-10 h-10'
+				/>
+			),
+			time: '9:30 AM',
+			message: 'That is a bummer. Have you tried using a different approach?',
+			status: 'Delivered',
+		},
+		{
+			name: 'Whitney Stevens',
+			image: (
+				<img
+					src={Avatar4}
+					className='w-10 h-10'
+				/>
+			),
+			time: '10:30 AM',
+			message:
+				'Yeah, I have been experimenting with different sales pitches and email, but nothing seems to work.',
+			status: 'Delivered',
+		},
+	])
+
 	return (
 		<>
 			<div className='flex gap-5'>
@@ -108,99 +164,78 @@ export const Messenger = () => {
 							<IoIosArrowDown />
 						</h1>
 					</div>
-					{messages.map((message, id) => (
-						<div
-							key={id}
-							className='flex justify-between my-2'
-						>
-							<div className='flex gap-2 cursor-pointer sm:text-sm'>
-								<div className='flex self-start'>{message.image}</div>
-								<div className='flex flex-col'>
-									<span className='sm:text-xs'>{message.name}</span>
-									<span className='text-sm text-gray-600'>
-										{message.message}
-									</span>
+					<div className='h-auto overflow-y-auto'>
+						{messages.map((message, id) => (
+							<div
+								key={id}
+								className='flex justify-between my-2'
+							>
+								<div
+									className='flex gap-2 cursor-pointer sm:text-sm'
+									onClick={console.log(id)}
+								>
+									<div className='flex self-start'>{message.image}</div>
+									<div className='flex flex-col'>
+										<span className='sm:text-xs'>{message.name}</span>
+										<span className='text-sm text-gray-600'>
+											{message.message}
+										</span>
+									</div>
+								</div>
+								<div className='flex self-start justify-end text-xs'>
+									{message.time}
 								</div>
 							</div>
-							<div className='flex self-start justify-end w-16 text-sm sm:text-xs'>
-								{message.time}
-							</div>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
 				{/* Messages */}
-				<div className='w-full p-4 bg-purple-500 rounded-xl'>
-					<div className='chat chat-start'>
-						<div className='chat-image avatar'>
-							<div className='w-10 rounded-full'>
-								<img
-									src={Avatar5}
-									alt='Avatar'
-								/>
-							</div>
+				<div className='flex flex-col w-full gap-2'>
+					<div className='flex items-center justify-between w-full px-4 py-2 bg-violet-200 rounded-xl'>
+						<h1 className='font-black'>Sales Team</h1>
+						<div className='flex gap-2'>
+							<LuVideo />
+							<LuPhoneCall />
+							<LuImage />
+							<TbBookUpload />
+							<TbSettings />
 						</div>
-						<div className='chat-header'>
-							Kevin Brown
-							<time className='text-xs opacity-50'>12:45</time>
+						<div className='flex items-center'>
+							<img
+								src={Avatar1}
+								className='w-8 h-8'
+								alt=''
+							/>
+							<img
+								src={Avatar2}
+								className='w-8 h-8'
+								alt=''
+							/>
+							<img
+								src={Avatar3}
+								className='w-8 h-8'
+								alt=''
+							/>
+							<p>+5</p>
 						</div>
-						<div className='chat-bubble'>Hey, guys! How are sales going?</div>
-						<div className='opacity-50 chat-footer'>Delivered</div>
 					</div>
-					<div className='chat chat-start'>
-						<div className='chat-image avatar'>
-							<div className='w-10 rounded-full'>
-								<img
-									src={Avatar2}
-									alt='Avatar'
-								/>
+					<div className='w-full h-auto p-4 overflow-y-auto bg-gradient-to-r from-purple-400 via-purple-500 to-fuchsia-300 rounded-xl'>
+						{chatBox.map((chat, id) => (
+							<div key={id}>
+								<div className='chat chat-start'>
+									<div className='chat-image avatar'>
+										<div className='w-10 rounded-full'>{chat.image}</div>
+									</div>
+									<div className='chat-header'>
+										{chat.name}
+										<time className='text-xs opacity-50'>{chat.time}</time>
+									</div>
+									<div className='chat-bubble'>{chat.message}</div>
+									<div className='opacity-50 chat-footer'>{chat.status}</div>
+								</div>
 							</div>
-						</div>
-						<div className='chat-header'>
-							Kevin Brown
-							<time className='text-xs opacity-50'>12:45</time>
-						</div>
-						<div className='chat-bubble'>
-							Not bad, but i think we could do better. We need to step up our
-							game.
-						</div>
-						<div className='opacity-50 chat-footer'>Delivered</div>
-					</div>
-					<div className='chat chat-start'>
-						<div className='chat-image avatar'>
-							<div className='w-10 rounded-full'>
-								<img
-									src={Avatar1}
-									alt='Avatar'
-								/>
-							</div>
-						</div>
-						<div className='chat-header'>
-							Kevin Brown
-							<time className='text-xs opacity-50'>12:45</time>
-						</div>
-						<div className='chat-bubble'>
-							That is a bummer. Have you tried using a different approach?
-						</div>
-						<div className='opacity-50 chat-footer'>Delivered</div>
-					</div>
-					<div className='chat chat-start'>
-						<div className='chat-image avatar'>
-							<div className='w-10 rounded-full'>
-								<img
-									src={Avatar4}
-									alt='Avatar'
-								/>
-							</div>
-						</div>
-						<div className='chat-header'>
-							Kevin Brown
-							<time className='text-xs opacity-50'>12:45</time>
-						</div>
-						<div className='bg-light-purple chat-bubble'>
-							Yeah, I have been experimenting with different sales pitches and
-							email, but nothing seems to work.
-						</div>
-						<div className='opacity-50 chat-footer'>Delivered</div>
+						))}
+						<div className='w-full bg-green-500'>hey</div>
 					</div>
 				</div>
 			</div>
