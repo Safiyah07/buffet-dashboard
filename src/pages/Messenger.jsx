dd61ea024c3904fc4bd88f8f4343ead0a38ce41c
Safiyah <safiyahmasud@gmail.com>
2023-07-16 23:26:33 +0100

    .

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -20,9 +20,17 @@ import Avatar4 from '../assets/Avatar4.png'
 import Avatar5 from '../assets/Avatar5.png'
 
 export const Messenger = () => {
+	const [bg, setBg] = useState(false)
+
+	const [tab, setTab] = useState(1)
+
+	const toggleTab = (id) => {
+		return setTab(id)
+	}
 	// eslint-disable-next-line no-unused-vars
 	const [messages, setMessages] = useState([
 		{
+			id: 1,
 			name: 'Henry Phillip',
 			image: (
 				<img
@@ -34,6 +42,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 2,
 			name: 'Seline Kim',
 			image: (
 				<img
@@ -45,6 +54,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 3,
 			name: 'Kevin Brown',
 			image: (
 				<img
@@ -56,6 +66,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 4,
 			name: 'Whitney Stevens',
 			image: (
 				<img
@@ -67,6 +78,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 5,
 			name: 'Mathew Pinnock',
 			image: (
 				<img
@@ -78,6 +90,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 6,
 			name: 'Thomas Kim',
 			image: (
 				<img
@@ -89,6 +102,7 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 		{
+			id: 7,
 			name: 'Jim Kim',
 			image: (
 				<img
@@ -100,6 +114,14 @@ export const Messenger = () => {
 			message: 'The spices are on their way',
 		},
 	])
+
+	const changeBg = (message, id) => {
+		const msgs = messages.findIndex((message) => message.id !== id)
+		const msg = messages.at(message.id)
+		console.log(msg)
+		console.log(msgs)
+		// setBg((prevState) => !prevState)
+	}
 
 	// eslint-disable-next-line no-unused-vars
 	const [chatBox, setChatBox] = useState([
@@ -157,8 +179,8 @@ export const Messenger = () => {
 
 	return (
 		<>
-			<div className='flex gap- h-[87.6vh]'>
-				{/* Contacts */}
+			<div className='flex gap- h-[87.6vh] text-black md:hidden sm:hidden'>
+				{/* Contacts for large screen */}
 				<div className='flex flex-col px-4 py-2 w-[42%] bg-violet-200 rounded-tl-xl rounded-b-xl sm:text-sm'>
 					<div className='flex items-center justify-between rounded'>
 						<input
@@ -175,16 +197,22 @@ export const Messenger = () => {
 							<FaAngleDown />
 						</h1>
 					</div>
-					<div className='h-auto overflow-y-auto'>
-						{messages.map((message, id) => (
+					{/* <div className='h-auto overflow-y-auto'> */}
+					<div
+						className={`${
+							bg === true ? 'bg-white' : ''
+						} h-auto overflow-y-auto `}
+					>
+						{messages.map((message) => (
 							<div
-								key={id}
-								className='flex justify-between my-2'
+								key={message.id}
+								// className='flex justify-between my-2 bg-white'
+								className={`${
+									bg === true ? '' : ''
+								} flex justify-between my-2 `}
+								onClick={changeBg}
 							>
-								<div
-									className='flex gap-2 cursor-pointer sm:text-sm'
-									onClick={console.log(id)}
-								>
+								<div className='flex gap-2 cursor-pointer sm:text-sm'>
 									<div className='flex self-start'>{message.image}</div>
 									<div className='flex flex-col'>
 										<span className='sm:text-xs'>{message.name}</span>
@@ -201,7 +229,7 @@ export const Messenger = () => {
 					</div>
 				</div>
 				{/* Messages */}
-				<div className='flex flex-col w-full gap-2'>
+				<div className='flex flex-col w-full gap-2 '>
 					<div className='flex justify-end w-full gap-1 px-4 py-3 bg-violet-200 rounded-br-xl'>
 						<TbMessageSearch />
 						<FaUserFriends />
@@ -277,6 +305,176 @@ export const Messenger = () => {
 							</div>
 						</div>
 					</div>
+				</div>
+			</div>
+			{/* Contacts for smaller screen */}
+			<div className='text-black lg:hidden md:block sm:block'>
+				<div className='flex items-center px-4 py-3 mb-3 bg-violet-200 rounded-xl'>
+					<h1 className='font-bold'>Messenger</h1>
+					<div className='flex justify-end w-full gap-1'>
+						<TbMessageSearch />
+						<FaUserFriends />
+						<FaRegStar />
+						<FaRegBell />
+						<TbSettings />
+					</div>
+				</div>
+				<div className='flex justify-between mb-3'>
+					<button
+						onClick={() => toggleTab(1)}
+						className={`${
+							tab === 1
+								? 'btn border-b-2 border-b-light-purple bg-white border-light-purple'
+								: 'btn btn-outline bg-transparent'
+						} `}
+					>
+						Messages
+					</button>
+
+					<button
+						onClick={() => toggleTab(2)}
+						className={`${
+							tab === 2
+								? 'btn border-b-2 border-b-light-purple bg-white border-light-purple'
+								: 'btn btn-outline bg-transparent'
+						} `}
+					>
+						Chats
+					</button>
+
+					<button
+						onClick={() => toggleTab(3)}
+						className={`${
+							tab === 3
+								? 'btn border-b-2 border-b-light-purple bg-white border-light-purple'
+								: 'btn btn-outline bg-transparent'
+						} `}
+					>
+						Calls
+					</button>
+				</div>
+				<div
+					className={`${
+						tab === 1 ? 'block' : 'hidden'
+					} h-auto overflow-y-auto `}
+				>
+					{messages.map((message) => (
+						<div
+							key={message.id}
+							// className='flex justify-between my-2 bg-white'
+							className={`${bg === true ? '' : ''} flex justify-between my-2 `}
+							onClick={changeBg}
+						>
+							<div className='flex gap-2 cursor-pointer sm:text-sm'>
+								<div className='flex self-start'>{message.image}</div>
+								<div className='flex flex-col'>
+									<span className='sm:text-xs'>{message.name}</span>
+									<span className='text-sm text-gray-600'>
+										{message.message}
+									</span>
+								</div>
+							</div>
+							<div className='flex self-start justify-end text-xs'>
+								{message.time}
+							</div>
+						</div>
+					))}
+				</div>
+				<div
+					className={`${
+						tab === 2 ? 'block' : 'hidden'
+					} flex flex-col w-full gap-2`}
+				>
+					<div className=''>
+						<div className='flex items-center justify-between w-full px-4 py-2 mb-2 bg-violet-200 rounded-xl'>
+							<h1 className='font-black'>Sales Team</h1>
+							<div className='flex gap-2'>
+								<TbVideo />
+								<LuPhoneCall />
+								<LuImage />
+								<TbBookUpload />
+								<TbSettings />
+							</div>
+							<div className='flex items-center'>
+								<img
+									src={Avatar1}
+									className='w-8 h-8'
+									alt=''
+								/>
+								<img
+									src={Avatar2}
+									className='w-8 h-8'
+									alt=''
+								/>
+								<img
+									src={Avatar3}
+									className='w-8 h-8'
+									alt=''
+								/>
+								<p>+5</p>
+							</div>
+						</div>
+						<div className='flex flex-col h-[72.4vh] justify-between w-full p-4 overflow-y-auto text-white bg-gradient-to-r from-purple-400 via-purple-500 to-fuchsia-300 rounded-xl'>
+							<div>
+								{chatBox.map((chat, id) => (
+									<div key={id}>
+										<div className='mb-2 chat chat-start'>
+											<div className='chat-image avatar'>
+												<div className='w-10 rounded-full'>{chat.image}</div>
+											</div>
+											<div className='chat-header'>
+												{chat.name}
+												<time className='text-xs opacity-50'> {chat.time}</time>
+											</div>
+											<div className='text-white chat-bubble bg-violet-200'>
+												{chat.message}
+											</div>
+										</div>
+									</div>
+								))}
+							</div>
+							<div className='w-full px-3 py-2 rounded-xl bg-gradient-to-r from-violet-400 to-violet-400'>
+								<div className='flex items-center justify-between'>
+									<div className='flex items-center gap-2'>
+										<img
+											src={Avatar1}
+											className='w-8'
+											alt=''
+										/>
+										Say something
+									</div>
+									<div className='flex items-center gap-2'>
+										<LuImage />
+										<FaMicrophone />
+										Send
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					className={`${
+						tab === 3 ? 'block' : 'hidden'
+					} h-auto overflow-y-auto `}
+				>
+					{messages.map((message) => (
+						<div
+							key={message.id}
+							// className='flex justify-between my-2 bg-white'
+							className={`${bg === true ? '' : ''} flex justify-between my-2 `}
+							onClick={changeBg}
+						>
+							<div className='flex items-center gap-2 cursor-pointer items sm:text-sm'>
+								<div className='flex self-start'>{message.image}</div>
+
+								<div className='sm:text-xs'>{message.name} called</div>
+							</div>
+							<div className='flex self-start justify-end text-xs'>
+								{message.time}
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</>
